3271b51810ecd217d61ae39cdca0ef82a496ab5d
Mihai Galos <mihaigalos@gmail.com>
2019-06-20 16:12:52 +0200

    Handle stray leading comma case

--- i3wm_wrapper.py
+++ i3wm_wrapper.py
@@ -19,18 +19,22 @@ example input from i3status:
 {"version":1}
 [
 [{"name":"ipv6","color":"#FF0000","markup":"none","full_text":"no IPv6"}]
+<subsequent lines:>
+,[{"name":"ipv6","color":"#FF0000","markup":"none","full_text":"no IPv6"}]
 """
 
 
 def constructOutputString():
     for line in sys.stdin:
+        if line[0] == ',':
+            line = line[1:]
         if "{\"version\":1}" != line.rstrip() and "[" != line.rstrip():
             jsonized_netatmo = {"name": "netatmo", "markup": "none",
                                 "full_text": getNetatmoData()}
             out_json = json.loads(line)
             out_json.insert(0, jsonized_netatmo)
 
-            sys.stdout.write(json.dumps(out_json))
+            sys.stdout.write(json.dumps(out_json) + ",")
 
         else:
             sys.stdout.write(line)
