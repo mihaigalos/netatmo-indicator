84e7204c617522f9f61e89f7ada0b2d4852c377f
Mihai Galos <mihaigalos@gmail.com>
2019-06-22 11:04:18 +0200

    Refactor i3status wrapper to separate repository

--- i3wm_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-i3status -c /etc/i3status.conf | while :
-do
-    read line
-    echo $line | python /home/mihai/git/netatmo-indicator/i3wm_wrapper.py /home/mihai/.netatmo-credentials.yaml || exit 1
-done
